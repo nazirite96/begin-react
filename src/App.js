c7222fcb382a871fa23065fcc17a53a79d5cfd57
nazirite96 <wkek805@naver.com>
2023-07-04 15:24:51 +0900

    공부
    공부

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from "./Wrapper";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = 'react';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24,
+        padding: '1rem'
+    }
+    return (
+        <Wrapper>
+            <Hello name="react" color="red" isSpecial></Hello>
+            <Hello color="pink"></Hello>
+            <div style={style}>{name}</div>
+            <div className="gray-box"></div>
+        </Wrapper>
+    );
 }
 
 export default App;
