a448787540fa8f6027ec1bbc3cf1fdae1848f611
nazirite96 <wkek805@naver.com>
2023-07-05 17:22:46 +0900

    공부
    공부

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import Hello from './Hello';
-import Wrapper from "./Wrapper";
+import React, {useRef} from 'react';
 import './App.css'
+import UserList from "./UserList";
 
 function App() {
-    const name = 'react';
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding: '1rem'
-    }
-    return (
-        <Wrapper>
-            <Hello name="react" color="red" isSpecial></Hello>
-            <Hello color="pink"></Hello>
-            <div style={style}>{name}</div>
-            <div className="gray-box"></div>
-        </Wrapper>
-    );
+    const users = [
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com'
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com'
+        }
+    ];
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        nextId.current += 1;
+    };
+
+    return <UserList users ={users}/>;
 }
 
 export default App;
